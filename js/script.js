82e41f3cd009c06f1f54e2b5c5bf59624e2f49f1
Agnieszka Matyja <agmatyja@gmail.com>
2019-02-19 12:04:35 +0100

    attempt to do the sidebar

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
-function toggleMenu(visible) {
-  document.querySelector('.menu').classList.toggle('show', visible)
-}
-document.querySelector('a.logo').addEventListener('click', function(e) {	
-  e.preventDefault();
-  toggleMenu()
-});
+	function toggleMenu(visible) {
+	  document.querySelector('.menu').classList.toggle('show', visible)
+	}
+	document.querySelector('a.logo').addEventListener('click', function(e) {	
+	  e.preventDefault();
+	  toggleMenu()
+	});
 	var ctx = document.getElementById('myChart').getContext('2d');
 
 	var chart = new Chart(ctx, {
